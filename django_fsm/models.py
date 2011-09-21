d2c3f4688c9977a50e9df12d599e40d2388c86d5
Ryan J Daw <ryan@mobify.me>
2011-09-21 08:12:58 +0800

    Adds audit trail.
    This adds an audit trail model (and associated signal handlers), that
    will keep a record of state transitions (as long as they go through
    djang-fsm).
    
    A few notes:
      * It requires the ContentType framework.
      * You must add this (and ContentType) to your installed Apps to
      register the models.
      * I've added the fieldname to the signal.
    
    A few open questions:
      * We use a really general post_save handler to save the audits, there
      may be a better way.
      * The audit logging is de-normalized somewhat, and thus isn't the best
      record of state changes, although it is simple to implement.

--- django_fsm/models.py
+++ django_fsm/models.py
@@ -1,4 +1,72 @@
-#-*- coding: utf-8 -*-
-"""
-Empty file, mark package as valid django application
-"""
+from django.db import models
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes import generic
+
+from django_fsm import signals
+from django.dispatch import receiver
+
+
+class Audit(models.Model):
+    """
+    The `Audit` model stores information about state transitions.
+    """
+
+    created_at = models.DateTimeField(auto_now_add=True,
+            help_text='Date and time of state change')
+
+    field = models.CharField(max_length=32,
+            verbose_name='Model field')
+
+    source = models.CharField(max_length=50,
+            verbose_name='Previous state')
+    
+    target = models.CharField(max_length=50,
+            verbose_name='New state')
+
+    content_type = models.ForeignKey(ContentType)
+    object_id = models.PositiveIntegerField()
+    
+    content_object = generic.GenericForeignKey('content_type', 'object_id')
+
+
+    def __repr__(self):
+        return "Audit(%s, '%s' => '%s')" \
+                % (repr(self.content_object), self.source, self.target)
+
+
+
+@receiver(signals.post_transition)
+def log_transition(sender, **kwargs):
+    """
+    The signal handler, `log_transition`, creates an audit log
+    entry when on `post_transition`.
+
+    This allow us to keep an audit trail for state changes.
+    """
+    instance = kwargs['instance']
+    source = kwargs['source']
+    target = kwargs['target']
+    field = kwargs['field']
+    
+    audit = Audit(field=field,
+            source=source,
+            target=target,
+            content_object=instance)
+
+    if hasattr(instance, '_django_fsm_audits'):
+        instance._django_fsm_audits.append(audit)
+    else:
+        instance._django_fsm_audits = [audit]
+
+@receiver(models.signals.post_save)
+def save_model_audits(sender, **kwargs):
+    """
+    The signal handler, `save_model_audits`, saves the audit logs
+    when the original model is saved.
+    """
+    instance = kwargs['instance']
+
+    if hasattr(instance, '_django_fsm_audits'):
+        for audit in instance._django_fsm_audits:
+            audit.save()
+
